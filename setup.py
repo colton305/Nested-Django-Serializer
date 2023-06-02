bc8ba3cf269cc5d2bd996076c1edf148ffc468ff
Colton Schneider <cschneider@c-a-i.net>
2023-06-02 11:33:37 -0600

    Added link to Github repo, changelog, and updated README.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="nested_django_serializer",
-    version="1.0.0",
+    version="1.1.0",
     author="Colton Schneider",
     description="Modifies the Django serializer such that it fully serializes foreign key relations.",
     long_description=long_description,
@@ -29,5 +29,8 @@ setup(
     python_requires=">=3.8",
     install_requires=["django"],
     license="MIT",
-    keywords="django json serializer"
+    keywords="django json serializer",
+    project_urls={
+        "Github": "https://github.com/colton305/nested-django-serializer"
+    }
 )
